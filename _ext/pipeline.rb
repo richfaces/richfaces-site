870913d6e6b5d06cffca874fa20fc5335a31e10d
Brian Leathem <bleathem@gmail.com>
2013-01-18 15:06:33 -0800

    Added the jboss community header

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,11 @@
+require File.join File.dirname(__FILE__), 'tweakruby'
+require_relative 'common'
+require_relative 'remotePartial'
 
 Awestruct::Extensions::Pipeline.new do
   # extension Awestruct::Extensions::Posts.new( '/news' ) 
   # extension Awestruct::Extensions::Indexifier.new
+
+  helper Awestruct::Extensions::RemotePartial
 end
 
